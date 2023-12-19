d7a5bd8401c01f480f620141177312b179dfd323
Gautam1998gk <gowtham1998gk@gmail.com>
2023-12-19 14:29:30 +0530

    main

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 
+
+/* 
 const MONGODB_URI = process.env.MONGODB_URI
 let cached = (global as any).mongoose || { conn: null, promise: null }
 export async function connectDb() {
@@ -10,13 +12,14 @@ export async function connectDb() {
         bufferCommands: false
     })
     cached.conn =await cached.promise
+    console.log(cached.conn);
     return cached.conn
 
-}
+} 
 
-/* let isConected = false
+let isConected = false
 
-export const connectToDB=async()=>{
+export const connectDb=async()=>{
  mongoose.set("strictQuery",true)
  if(!process.env.DATABASE_URI) return console.log("Mongoose Uri required");
  if(isConected) return console.log("using existing data base");
@@ -31,4 +34,24 @@ export const connectToDB=async()=>{
     console.log(error);
     
  }
-} */
+}*/
+
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+let cached = (global as any).mongoose || { conn: null, promise: null };
+
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
+
+  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    dbName: 'evently',
+    bufferCommands: false,
+  })
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+}
--- src/lib/actions/userAction.ts
+++ src/lib/actions/userAction.ts
@@ -1,60 +1,82 @@
-"use server"
+'use server'
 
-import { connectDb } from "@/database"
-import { CreateUserParams, UpdateUserParams } from "../../../types"
-import { handleError } from "../utils"
-import User from "@/database/models/userModel"
-import Event from "@/database/models/eventModel"
-import { revalidatePath } from "next/cache"
-import Order from "@/database/models/orderModel"
+import { revalidatePath } from 'next/cache'
 
-export const createUser=async(user:CreateUserParams)=>{
-    try {
-        await connectDb()
-        const newUser =await  User.create(user)
-        return JSON.parse(JSON.stringify(newUser))
-    } catch (error) {
-        handleError(error)
-    }
+import { handleError } from '@/lib/utils'
+import { connectToDatabase } from '@/database'
+import { CreateUserParams, UpdateUserParams } from '../../../types'
+import User from '@/database/models/userModel'
+import Event from '@/database/models/eventModel'
+import Order from '@/database/models/orderModel'
+
+
+
+export async function createUser(user: CreateUserParams) {
+  try {
+    await connectToDatabase()
+
+    const newUser = await User.create(user)
+    return JSON.parse(JSON.stringify(newUser))
+  } catch (error) {
+    handleError(error)
+  }
 }
 
-export const updateUser =async (id:string,user:UpdateUserParams) => {
-    try {
-        await connectDb()
-        const updateuser=await User.findByIdAndUpdate({_id:id},user,{new:true})
-        if (!updateuser) throw new Error('User update failed')
-        return JSON.parse(JSON.stringify(updateuser))
-    } catch (error) {
-        handleError(error)
-    }
+export async function getUserById(userId: string) {
+  try {
+    await connectToDatabase()
+
+    const user = await User.findById(userId)
+
+    if (!user) throw new Error('User not found')
+    return JSON.parse(JSON.stringify(user))
+  } catch (error) {
+    handleError(error)
+  }
 }
 
-export const deleteUser =async (id:string) => {
-    try {
-        await connectDb()
-        const userToDelete = await User.findOne({ id })
+export async function updateUser(clerkId: string, user: UpdateUserParams) {
+  try {
+    await connectToDatabase()
 
-        if (!userToDelete) {
-          throw new Error('User not found')
-        }
-      // Unlink relationships
+    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, { new: true })
+
+    if (!updatedUser) throw new Error('User update failed')
+    return JSON.parse(JSON.stringify(updatedUser))
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+export async function deleteUser(clerkId: string) {
+  try {
+    await connectToDatabase()
+
+    // Find user to delete
+    const userToDelete = await User.findOne({ clerkId })
+
+    if (!userToDelete) {
+      throw new Error('User not found')
+    }
+
+    // Unlink relationships
     await Promise.all([
-        // Update the 'events' collection to remove references to the user
-        Event.updateMany(
-          { _id: { $in: userToDelete.events } },
-          { $pull: { organizer: userToDelete._id } }
-        ),
-  
-        // Update the 'orders' collection to remove references to the user
-        Order.updateMany({ _id: { $in: userToDelete.orders } }, { $unset: { buyer: 1 } }),
-      ])
-  
-      // Delete user
-      const deletedUser = await User.findByIdAndDelete(userToDelete._id)
-      revalidatePath('/')
-  
-      return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null
-    } catch (error) {
-        handleError(error)
-    }
+      // Update the 'events' collection to remove references to the user
+      Event.updateMany(
+        { _id: { $in: userToDelete.events } },
+        { $pull: { organizer: userToDelete._id } }
+      ),
+
+      // Update the 'orders' collection to remove references to the user
+      Order.updateMany({ _id: { $in: userToDelete.orders } }, { $unset: { buyer: 1 } }),
+    ])
+
+    // Delete user
+    const deletedUser = await User.findByIdAndDelete(userToDelete._id)
+    revalidatePath('/')
+
+    return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null
+  } catch (error) {
+    handleError(error)
+  }
 }
