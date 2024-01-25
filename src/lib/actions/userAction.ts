8400442e9bbd7c738c9755de6231ded49ba4e221
Pelluru32 <gowthampelluru@gmail.com>
2024-01-25 08:03:36 +0530

    main

--- src/lib/actions/userAction.ts
+++ src/lib/actions/userAction.ts
@@ -14,10 +14,10 @@ import Order from '@/database/models/orderModel'
 export async function createUser(user: CreateUserParams) {
   try {
     await connectToDatabase()
-
     const newUser = await User.create(user)
     return JSON.parse(JSON.stringify(newUser))
   } catch (error) {
+    console.log(error)
     handleError(error)
   }
 }
